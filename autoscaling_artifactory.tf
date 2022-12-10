be2271f2c58ab36b5120c5edd6cf4b21f26edcda
Helgi Gunnarsson <helgiorn@gmail.com>
2022-12-10 22:45:08 +0100

    module testing

--- autoscaling_artifactory.tf
+++ autoscaling_artifactory.tf
@@ -16,17 +16,17 @@ resource "aws_autoscaling_attachment" "artifactory" {
 }
 
 resource "aws_launch_template" "artifactory_server" {
-  name                    = "artifactory_server"
-  instance_type           = "t2.micro"
-  image_id                = "ami-test"
-  ebs_optimized           = true
-  disable_api_stop        = true
-  disable_api_termination = true
-  vpc_security_group_ids  = ["data"]
-  instance_initiated_shutdown_behavior  = "terminate"
+  name                                 = "artifactory_server"
+  instance_type                        = "t2.micro"
+  image_id                             = "ami-test"
+  ebs_optimized                        = true
+  disable_api_stop                     = true
+  disable_api_termination              = true
+  vpc_security_group_ids               = ["data"]
+  instance_initiated_shutdown_behavior = "terminate"
 
   block_device_mappings {
-    device_name   = "/dev/sda1"
+    device_name = "/dev/sda1"
 
     ebs {
       volume_size = 30
@@ -43,12 +43,12 @@ resource "aws_launch_template" "artifactory_server" {
   }
   network_interfaces {
     associate_public_ip_address = false
-    delete_on_termination = true 
-    subnet_id = ""
-    vpc_id = ""
-    security_groups = ""
+    delete_on_termination       = true
+    subnet_id                   = ""
+    vpc_id                      = ""
+    security_groups             = ""
   }
-  
+
   tag_specifications {
     resource_type = "instance"
 
